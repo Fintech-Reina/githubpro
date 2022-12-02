16719d7d90ef0484cf4444050bf5db7c2ea81c39
Reinadjay1131 <94231253+Reinadjay1131@users.noreply.github.com>
2022-12-01 22:49:30 -0500

    Git right up commit

---
+++ github.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+<h2> Git Hub </h2>
+echo "github is an source code manager"
+echo "it is integrated with git, which supports data integrity"
+echo "As a devops engineer, i serve as the SME, enabling an environment where developers have secured and well configured environments"
+echo "git supports rollout and roll backs"
+echo "git is a DVCS"
