6c07a2b0c49503ce4f212a7c1918c064dd61afd6
reinadjay1131 <julietibeachu3@icloud.com>
2022-12-02 03:42:47 +0000

    code commit

---
+++ githubpro/githubpro1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+<h1> Welcome to Landmark Technologies </h1>
+echo "We are about to become millionaires from Landmark University"
+echo "Running codes and helping organisations deploy applications with ease"
