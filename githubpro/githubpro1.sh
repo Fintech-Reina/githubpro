c78086288fa5b844843ab0b9c90f910f94529bc1
Reinadjay1131 <94231253+Reinadjay1131@users.noreply.github.com>
2022-12-01 23:03:42 -0500

    file updated

--- githubpro/githubpro1.sh
+++ githubpro/githubpro1.sh
@@ -2,3 +2,4 @@
 <h1> Welcome to Landmark Technologies </h1>
 echo "We are about to become millionaires from Landmark University"
 echo "Running codes and helping organisations deploy applications with ease"
+echo "Hello World"
